0b47b6cea0ec7822049c8534f9f4179c46213d49
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2023-12-11 21:19:27 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function searchSubject() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("subjectInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("subject");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
