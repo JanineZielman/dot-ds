399e84a5a2a014e6c683995e821a9d16b9e40349
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2024-01-09 14:35:38 +0100

    update

--- script.js
+++ script.js
@@ -23,62 +23,68 @@ function getInput() {
   document.getElementById('what-link').href = current + '&what=' + val;
 }
 
-let when,condition,criteria; 
-
-function getWhen() {
-  var input, val, current;
-  input = document.getElementById("whenInput");
-  val = input.value.toLowerCase().replaceAll(' ', '-');
+function getWhen(event) {
+  var input, current;
+  input = event.target.innerText.replaceAll(' ', '-').toLowerCase();
   current = window.location.search;
-  when = '&when=' + val;
+  window.location.href = current + '&when=' + input;
 }
 
-// function getCondition() {
-//   var input, val, current;
-//   input = document.getElementById("conditionInput");
-//   val = input.value.toLowerCase().replaceAll(' ', '-');
-//   current = window.location.search;
-//   condition = '&condition=' + val;
-//   document.getElementById('when-how-link').href = current + when + condition + criteria;
-// }
-
-function conditionSearch(event) {
+function searchWhen() {
   var input, filter, ul, li, a, i, txtValue;
-  input = document.getElementById("conditionInput");
+  input = document.getElementById("whenInput");
   filter = input.value.toUpperCase();
-  ul = document.getElementById("condition");
+  ul = document.getElementById("when");
   li = ul.getElementsByTagName("li");
   for (i = 0; i < li.length; i++) {
       txtValue = li[i].textContent || li[i].innerText;
-      li[i].addEventListener("click", function (event) {
-        input.value = event.target.innerText;
-      });
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
           li[i].style.display = "";
-          if(event.key == 'Enter'){
-            current = window.location.search;
-            condition = '&condition=' + li[i].innerText.toLowerCase();
-            input.value = txtValue;
-          }
       } else {
           li[i].style.display = "none";
       }
   }
 }
 
+function getHow(event) {
+  var input, current;
+  input = event.target.innerText.replaceAll(' ', '-').toLowerCase();
+  current = window.location.search;
+  window.location.href = current + '&how=' + input;
+}
+
+function searchHow() {
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("howInput");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("how");
+  li = ul.getElementsByTagName("li");
+  for (i = 0; i < li.length; i++) {
+      txtValue = li[i].textContent || li[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          li[i].style.display = "";
+      } else {
+          li[i].style.display = "none";
+      }
+  }
+}
 
 function getCriteria() {
   var input, val, current;
   input = document.getElementById("criteriaInput");
   val = input.value.toLowerCase().replaceAll(' ', '-');
   current = window.location.search;
-  criteria = '&criteria=' + val;
-  document.getElementById('when-how-link').href = current + when + condition + criteria;
+  document.getElementById('criteria-link').href = current + '&criteria=' + val;
 }
+
+
+
 
 window.addEventListener("load", (event) => {
   document.getElementById('what-wrapper').style.display = 'none';
-  document.getElementById('when-how-wrapper').style.display = 'none';
+  document.getElementById('when-wrapper').style.display = 'none';
+  document.getElementById('how-wrapper').style.display = 'none';
+  document.getElementById('criteria-wrapper').style.display = 'none';
   document.getElementById('result').style.display = 'none';
 
   const queryString = window.location.search;
@@ -86,22 +92,37 @@ window.addEventListener("load", (event) => {
   const subject = urlParams.get('subject')
   const what = urlParams.get('what')
   const when = urlParams.get('when')
-  const condition = urlParams.get('condition')
+  const how = urlParams.get('how')
   const criteria = urlParams.get('criteria')
+
   
   if (subject){
     document.getElementById('subject-wrapper').style.display = 'none';
     document.getElementById('what-wrapper').style.display = 'block';
+    document.getElementById('topbar').innerHTML = `${subject}`;
   }
   if(what){
     document.getElementById('what-wrapper').style.display = 'none';
-    document.getElementById('when-how-wrapper').style.display = 'block';
+    document.getElementById('when-wrapper').style.display = 'block';
+    document.getElementById('topbar').innerHTML = `${subject} • ${what}`;
   }
   if(when){
-    document.getElementById('when-how-wrapper').style.display = 'none';
+    document.getElementById('when-wrapper').style.display = 'none';
+    document.getElementById('how-wrapper').style.display = 'block';
+    document.getElementById('topbar').innerHTML = `${subject} • ${what} • ${when}`;
+  }
+  if(how){
+    document.getElementById('how-wrapper').style.display = 'none';
+    document.getElementById('criteria-wrapper').style.display = 'block';
+    document.getElementById('topbar').innerHTML = `${subject} • ${what} • ${when} • ${how}`;
+  }
+  if(criteria){
+    document.getElementById('criteria-wrapper').style.display = 'none';
     document.getElementById('result').style.display = 'block';
-
-    document.getElementById('final-text').innerHTML = when.replaceAll('-', ' ') + ' students should ' + ' ' + criteria.replaceAll('-', ' ')  + ' ' + condition.replaceAll('-', ' ')  + ' ' + what.replaceAll('-', ' ')
+    document.getElementById('topbar').style.display = 'none';
+    document.getElementById('final-text').innerHTML = when.replaceAll('-', ' ') + ' students should ' + ' ' + criteria.replaceAll('-', ' ')  + ' ' + how.replaceAll('-', ' ')  + ' ' + what.replaceAll('-', ' ')
   }
 
+
+  
 });
